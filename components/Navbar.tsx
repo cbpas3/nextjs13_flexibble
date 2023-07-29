219713382681ecc74f0b33060ccbe1637c8d713c
Cy <notcyanpascual@gmail.com>
2023-07-29 17:45:39 +0800

    current time: 0:58:16

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
-    const session = {}
+    const session = null;
     return (
         <nav className='flexBetween navbar'>
             <div className='flex-1 flexStart gap-10'>
